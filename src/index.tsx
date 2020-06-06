ded6886eabd0798ad874080b6a3d716f25242a08
colorfulmdx2 <colorfulmdx2@gmail.com>
2020-06-06 12:07:35 +0300

    Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-
-
-
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
+import store from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import {rerenderEntireTree} from "./render";
+import {BrowserRouter} from "react-router-dom";
 
 
 
+let rerenderEntireTree = (state:any) => {
+    ReactDOM.render(
 
-rerenderEntireTree(state)
+        <BrowserRouter>
+            <App state={store.getState()}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}
+
+            />
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+
+}
+
+rerenderEntireTree(store.getState())
+
+store.subscriber(rerenderEntireTree)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,54 +1,70 @@
 
-import {rerenderEntireTree} from "../render";
+let store = {
+    _state: {
+        navigationPage: {
+            friendIconData: [
+                {name: 'Dima'},
+                {name: 'Kate'},
+                {name: 'Mario'}
+            ]
+        },
+        profilePage: {
+            postData: [
+                {id: 1, like: '12', message: 'Hello world'},
+                {id: 2, like: '16', message: 'Learning React....'},
+                {id: 3, like: '19', message: 'Fucked up...'}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            messagesData: [
+                {id: 1, messages: 'Holla'},
+                {id: 2, messages: 'Im Lolaaaaaa!!'},
+                {id: 3, messages: 'Im a pirate QUEEN!!'},
+                {id: 4, messages: 'Joking...)!!'},
+            ],
+            dialogsData: [
+                {id: 1, name: 'Dima'},
+                {id: 2, name: 'Kate'},
+                {id: 3, name: 'Mario'},
+                {id: 4, name: 'Jovanni'},
+                {id: 5, name: 'Franchesco'},
+                {id: 6, name: 'Karin'},
+            ]
+        },
+    },
+    getState () {
+        return this._state
+    },
+    _callSubscriber (state:any)  {
+        console.log('State has been changed')
+    },
+    addPost ()  {
+        let newPost = {
+            id: 4,
+            message: this._state.profilePage.newPostText,
+            like: '0'
+        }
+        this._state.profilePage.postData.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state)
+    },
+    updateNewPostText  (newText:any)  {
+        this._state.profilePage.newPostText = newText
+        this._callSubscriber(this._state)
+    },
+    subscriber  (observer:any)  {
+        this._callSubscriber = observer
+    },
 
 
-let state = {
-    navigationPage: {
-        friendIconData: [
-            {name: 'Dima'},
-            {name: 'Kate'},
-            {name: 'Mario'}
-        ]
-    },
-    profilePage: {
-        postData: [
-            {id: 1, like: '12', message: 'Hello world'},
-            {id: 2, like: '16', message: 'Learning React....'},
-            {id: 3, like: '19', message: 'Fucked up...'}
-        ],
-        newPostText: ''
-    },
-    dialogsPage: {
-        messagesData: [
-            {id: 1, messages: 'Holla'},
-            {id: 2, messages: 'Im Lolaaaaaa!!'},
-            {id: 3, messages: 'Im a pirate QUEEN!!'},
-            {id: 4, messages: 'Joking...)!!'},
-        ],
-        dialogsData: [
-            {id: 1, name: 'Dima'},
-            {id: 2, name: 'Kate'},
-            {id: 3, name: 'Mario'},
-            {id: 4, name: 'Jovanni'},
-            {id: 5, name: 'Franchesco'},
-            {id: 6, name: 'Karin'},
-        ]
-    },
+
 }
 
-export let addPost = () => {
-    let newPost = {
-        id: 4,
-        message: state.profilePage.newPostText,
-        like: '0'
-    }
-    state.profilePage.postData.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
 
-export let updateNewPostText = (newText:any) => {
-       state.profilePage.newPostText = newText
-    rerenderEntireTree(state)
-}
-export default state
+
+
+
+
+
+export default store
